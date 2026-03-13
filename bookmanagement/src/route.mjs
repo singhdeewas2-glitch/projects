2b2a0c7416a5d060670b90a7e18f95ce19004ea9
Abhijit-Singh <abhijits1280@gmail.com>
2026-03-13 19:51:54 +0530

    Update route.mjs

--- bookmanagement/src/route.mjs
+++ bookmanagement/src/route.mjs
@@ -7,14 +7,17 @@ import * as booksController from './controllers/bookControllers.mjs';
 const router = express.Router();
 router.post('/login', login);
 router.post('/register', register);
+
+router.get('/books', verifyToken, booksController.getAll);
+router.get('/books/:id', verifyToken, booksController.getById);
+router.post('/books', verifyToken, booksController.add);
+router.put('/books/:id', verifyToken, booksController.update);
+router.delete('/books/:id', verifyToken, booksController.remove);
+
 router.post('/books/:bookId/review', verifyToken, createReview);
 router.put('/books/:bookId/review/:reviewId', verifyToken, updateReview);
 router.delete('/books/:bookId/review/:reviewId',verifyToken, deleteReview);
-router.get('/books', Auth.verifyToken, booksController.getAll);
-router.get('/books/:id', Auth.verifyToken, booksController.getById);
-router.post('/books', Auth.verifyToken, booksController.add);
-router.put('/books/:id', Auth.verifyToken, booksController.update);
-router.delete('/books/:id', Auth.verifyToken, booksController.remove);
+
 router.post("/frontend",verifyToken ,uploadImage)
 
 export default router;
